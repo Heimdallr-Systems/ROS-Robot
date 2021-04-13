62848a6095b261ec257c7ebf289c0528e73d5ddf
Heimdallr Robot <77315387+heimdallr-robot@users.noreply.github.com>
2021-04-12 20:13:00 -0700

    set up pan-tilt node

---
+++ src/pantilt_control/src/drive_pantilt.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float32
+import math
+pi=math.pi
+pan_angle=0.0
+tilt_angle=0.0
+tilt_offset=pi/2.0+pi/12.0
+pan_increment=0.5*pi/180.0
+pan_max=pi/2.0
+tilt_max=pi/6.0
+tilt_increment=4.0*pan_increment*(tilt_max/pan_max)
+pan_dir=1.0
+tilt_dir=1.0
+wait_cnt=0
+loop_rate=20
+wait_max=2*loop_rate
+full_rotation_cnt=0
+if __name__=='__main__':
+  try:
+    rospy.init_node('pantilt',anonymous=True)
+    pan=rospy.Publisher('pan_target',Float32,queue_size=100)
+    tilt=rospy.Publisher('tilt_target',Float32,queue_size=100)
+    rate=rospy.Rate(loop_rate)
+    while not rospy.is_shutdown():
+#        print("tilt overflow, loop count: " + str(loop_cnt))
+      if(full_rotation_cnt%2==0):
+        if(abs(pan_angle)>=pan_max):
+          pan_dir=-pan_dir
+        if(abs(tilt_angle)>=tilt_max):
+          tilt_dir=-tilt_dir
+        if(abs(pan_angle)<=0.5*pi/180.0):
+          full_rotation_cnt+=1
+          print("pan overflow, rotation count: " + str(full_rotation_cnt))
+
+        pan_angle+=pan_dir*pan_increment
+        tilt_angle+=tilt_dir*tilt_increment
+        pan.publish(pan_angle)
+        tilt.publish(tilt_angle+tilt_offset)
+        print("Pan angle: " + str(pan_angle*180.0/pi) + ", tilt angle: " + str(tilt_angle*180.0/pi))
+      else:
+        wait_cnt+=1
+        if(wait_cnt==wait_max):
+          wait_cnt=0
+          full_rotation_cnt+=1
+      rate.sleep()
+  except rospy.ROSInterruptException:
+    pass
+
+
---
+++ src/pantilt_control/src/pca9685_controller.py
@@ -0,0 +1,56 @@
+from __future__ import division
+import time
+import math
+# Import the PCA9685 module.
+import Adafruit_PCA9685
+
+
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+# Helper function to make setting a servo pulse width simpler.
+def set_servo_pulse(channel, pulse,pwm):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 60       # 60 Hz
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
+
+def init_pca9685():
+    # Initialise the PCA9685 using the default address (0x40).
+    pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)
+
+    # Set frequency to 60hz, good for servos.
+    pwm.set_pwm_freq(60)
+    return pwm
+def rad2pulse(rad):
+    # Convert radians to servo pulses
+    pstart = 150 # us
+    pend = 1000 # us
+    pmiddle = (pend-pstart)/2+pstart # us
+    radrange=270*math.pi/180
+    return int(((pend-pstart)/radrange*rad+pmiddle)/2)
+def drive_servos(pwm,servo_pos,i):
+    # Servo pos is given in radians
+    #for i in range(0,min(len(servo_pos),15)):
+    pwm.set_pwm(i,0,rad2pulse(servo_pos))
+    feedback = "driving port " + str(i) + " to " + str(rad2pulse(servo_pos))
+    print(feedback)
+    return [pwm,feedback]
+'''pwm=init_pca9685()
+print('Moving servo on channel 0, press Ctrl-C to quit...')
+while True:
+    print("forward")
+    # Move servo on channel O between extremes.
+    pwm=drive_servos(pwm,0,0)
+    #pwm=drive_servos(pwm,[150,150,150])
+    time.sleep(1)
+    print("back")
+    pwm=drive_servos(pwm,math.pi/4,0)
+    #pwm=drive_servos(pwm,[600,600,600])
+    time.sleep(1)
+'''
