d4556b01c8514be8aa0dd335a8fd4373ce2ffa5c
Heimdallr Robot <77315387+heimdallr-robot@users.noreply.github.com>
2021-04-13 17:47:55 -0700

    update servo driver code

--- src/servo_control/src/servo_feedback_measured.cpp
+++ src/servo_control/src/servo_feedback_measured.cpp
@@ -6,22 +6,21 @@
 #include "servo_adc.h"
 #define PI 3.1415926535
 // ADC stuff
-ADC_SPI_T adcDevice;
-ADC_GPIO_T adcGPIO;
-uint16_t adcVals[15];
 int main(int argc, char **argv) {
+uint16_t adcVals[15];
+
   ros::init(argc,argv,"servo_feedback_measured");
   ros::NodeHandle n;
   ros::Publisher target_pub = n.advertise<servo_control::MotorVector>("servo_feedback",1000);
   ros::Rate loop_rate(5);
-  if(setupADC(&adcDevice,&adcGPIO)) {
-    std::cout << "ADC init error. Closing Servo Feedback node.\n";
-    return 0;
-  }
+//  if(setupADC(&adcDevice,&adcGPIO)) {
+//    std::cout << "ADC init error. Closing Servo Feedback node.\n";
+//    return 0;
+//  }
   servo_control::MotorVector msg;
 
   while(ros::ok()) {
-    if(readADC(&adcDevice, &adcGPIO,adcVals)) {
+    if(readADC(adcVals)) {
       std::cout << "ADC read error. Closing Servo Feedback node.\n";
       break;
     }
@@ -37,20 +36,20 @@ int main(int argc, char **argv) {
      if(adcVals[i]!=0) {
        nonzerocnt++;
      }
-//     std::cout << i <<": " << adcVals[i] << ", ";
-     msg.data[i]=adcVals[i];//*3*PI/2.0/1024.0-PI/2.0;
+     std::cout << i <<": " << adcVals[i] << ", ";
+     msg.data[i]=(adcVals[i]-487)*3*PI/2.0/1024.0;
    }
    if(cancel==0 &&nonzerocnt!=0) {
    target_pub.publish(msg);
 //   std::cout<<msg.data[0];
-//   std::cout << "\n";
+   std::cout << "\n";
    }
    loop_rate.sleep();
   }
 
-  if(closeADC(&adcDevice,&adcGPIO)) {
-    std::cout << "ADC close error.\n";
-   return -1;
-  }
+//  if(closeADC(&adcDevice,&adcGPIO)) {
+//    std::cout << "ADC close error.\n";
+//   return -1;
+//  }
   return 0;
 }
--- src/servo_driver/src/pca_driver.py
+++ src/servo_driver/src/pca_driver.py
@@ -9,23 +9,28 @@ import math
 def callback(data):
   # Callback fcn
 #  print(data[0])
-  for i in range(0,len(data)-2):
-    print("Driving servo "+str(i) + " to \"" +str(data[i]) + "\" radians.")
-    pos=data[i]
+  inv=[5, 7,9, 11]
+  for i in range(0,13):
+    print("Driving servo "+str(i) + " to \"" +str(data.data[i]*180.0/math.pi) + "\" degrees.")
+    if(i-1 in inv):
+      pos=-data.data[i]
+    else:
+      pos=data.data[i]
     a=pca.drive_servos(pwm,pos,i)
+    #a=pca.drive_servos(pwm,0,4)
 #    print(a[1])
 #    time.sleep(.1)
 def pan_callback(data):
   pca.drive_servos(pwm,data.data,12)
-
+  print("pan")
 def tilt_callback(data):
   pca.drive_servos(pwm,data.data,13)
-
+  print("tilt")
 def servo_target_listener():
   pos=0
   mult=5
   rospy.init_node('servo_driver_target_listener',anonymous=True)
-  #rospy.Subscriber("servo_goal_positions",MotorVector,callback)
+  rospy.Subscriber("servo_goal_positions",MotorVector,callback)
   rospy.Subscriber("pan_target",Float32,pan_callback)
   rospy.Subscriber("tilt_target",Float32,tilt_callback)
    
@@ -38,12 +43,12 @@ def poseTest():
   a=str(input("Press 1 to initialize port zero, 2 to stand up: "))
   if(a=='1'):
     print("driving servo 1 to zero")
-    for i in range(0,14):
+    for i in range(0,12):
       q=pca.drive_servos(pwm,servo_poses[i],i)
     print("at initial pose")
   elif(a=='2'):
     print("driving robot to an initial position")
-    for i in range(0,14):
+    for i in range(0,12):
       pca.drive_servos(pwm,servo_poses_1[i],i)
     time.sleep(2)
     a=pca.drive_servos(pwm,pi/4,4)
@@ -65,15 +70,43 @@ if __name__ == '__main__':
   print("Note: Initializing PCA9685 servo driver...")
   pwm=pca.init_pca9685()
   pi=3.1415926535
-  servo_target_listener()
-  servo_poses=[pi/4.0,-pi/4.0, -pi/4.0, pi/4.0, 0,0,0,0,pi/2.0, pi/2.0,pi/2.0,pi/2.0,pi/4.0,0]
+  servo_poses=[pi/4.0,-pi/4.0, -pi/4.0, pi/4.0, 0,0,0,0,pi/2.0, pi/2.0,pi/2.0,pi/2.0,-pi/12.0,pi/2.0+pi/12.0]
   servo_poses_1=[pi/4.0,-pi/4.0-pi/3.0, -pi/4.0+pi/4.0, pi/6.0, pi/6.0,pi/6.0,pi/6.0,pi/6.0,pi/2.0, pi/2.0+pi/4.0,pi/2.0+pi/4.0,pi/2.0+pi/3.0,pi/4.0,0]
+  step1=[pi/4.0,-pi/4.0,-pi/4.0,pi/4.0, -pi/4.0,pi/4.0,-pi/4.0,pi/4.0,3.0*pi/4.0,-3.0*pi/4.0,3.0*pi/4.0,-3.0*pi/4.0,0,0]
+  step2=[0.7854,-0.7854,-0.7854,0.7854,0.2542,-0.2542,0.2542,-0.2542, 1.5095,-1.5095,1.5095,-1.5095,0,0]
   ports=[0, 1, 2, 3,4,5,6,7,8,9,10,11,12,13]
   for i in range(0,14):
     q=pca.drive_servos(pwm,0,i)
 #    input("press something")
+  time.sleep(2)
+  inv=[5,7,9,11]
+  for i in range(0,14):
+    if(i in inv):
+      q=pca.drive_servos(pwm,-step1[i],i)
+    else:
+      q=pca.drive_servos(pwm,step1[i],i)
+  time.sleep(5)
+  for i in range(0,14):
+    if(i in inv):
+      q=pca.drive_servos(pwm,-step2[i],i)
+    else:
+      q=pca.drive_servos(pwm,step2[i],i)
+  time.sleep(5)
+  # Tilt around to make RTAB work
+  offset=pi/2.0+pi/12.0
+  for i in range(0,9):
+    pca.drive_servos(pwm,float(i)*5.0*pi/180.0+offset,14)
+    time.sleep(0.1)
+  for i in range(0,18):
+    pca.drive_servos(pwm,float(9-i)*5.0*pi/180.0+offset,14)
+    time.sleep(0.1)
+  for i in range(0,9):
+    pca.drive_servos(pwm,float(i-9)*5.0*pi/180.0+offset,14)
+    time.sleep(0.1)
+
+  servo_target_listener()
   while(not rospy.is_shutdown()):
-     poseTest()
-#     rospy.spin()
+#     poseTest()
+     rospy.spin()
 #    servo_target_listener()
 
